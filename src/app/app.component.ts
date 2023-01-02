f13064a38ee6a55a6a0a1b770dfa9d904d442d06
Harsha Reddy <harsha.reddy@ecolab.com>
2023-01-02 15:29:35 +0530

    rejoin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+
+import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +12,32 @@ export class AppComponent implements OnInit {
   newPlayer: string;
   Players: string[] = [];
   distributor: number = 0;
-
-
+  closeResult = '';
+  totalpoints: number = 0;
+  rejoinPlayers: number[] = [];
+  rejoinedPlayers: number[] = [];
   Score: any = {};
+
+  ppp: number[] = [];
+
+  @ViewChild('totalPoints', { static: true }) totalPointsModal: TemplateRef<any>;
+  @ViewChild('rejoin', { static: true }) rejoinModal: TemplateRef<any>;
+
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
     this.Players = JSON.parse(localStorage.getItem('players')) ? JSON.parse(localStorage.getItem('players')) : [];
     this.Score = JSON.parse(localStorage.getItem('score'));
+    this.totalpoints = localStorage.getItem('totalScore') ? Number(localStorage.getItem('totalScore')) : 0;
     if (!this.Score) {
       this.InitScore();
     }
+    if (this.totalpoints == 0)
+      this.getTotalpoints();
     console.log(this.Players);
     console.log(this.Score);
+
   }
 
   AddScore() {
@@ -32,15 +48,76 @@ export class AppComponent implements OnInit {
         (<HTMLInputElement>document.getElementById(this.Players[i])).value = "";
       }
       this.Score.Games = this.Score.Games + 1;
+
+      this.checkRejoin();
       this.updateScore();
-      this.updateDistributor();
+      this.nextDistributor();
     }
     else {
       alert('Add players before adding score');
     }
+
+  }
+  checkRejoin() {
+
+    this.rejoinPlayers = [];
+    var highestScore = -1;
+    for (var i = 0; i < this.Players.length; i++) {
+      var sum = this.sum(this.Players[i]);
+      if (sum >= this.totalpoints) {
+        this.rejoinPlayers.push(i);
+        continue;
+      }
+      if (sum < this.totalpoints && highestScore < sum) {
+        highestScore = sum;
+      }
+    }
+
+    console.log('Highest Score', highestScore);
+
+    if (this.rejoinPlayers.length == 0) { return; }
+
+
+    this.modalService.open(this.rejoinModal, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      () => {
+        this.rejoinPlayers = []; 
+        this.rejoin(this.rejoinedPlayers, highestScore);
+      },
+      () => {
+        this.checkRejoin();
+      },
+    );
+  }
+  onChange(p: number, isChecked: boolean) {
+    if (isChecked) {
+      this.rejoinedPlayers.push(p);
+    } else {
+      let index = this.rejoinedPlayers.indexOf(p);
+      this.rejoinedPlayers.splice(index, 1);
+    }
+    this.rejoinedPlayers = [...new Set(this.rejoinedPlayers)];
   }
 
-  updateDistributor() {
+  rejoin(players: number[], highestScore: number) {
+
+    if(players == null || players.length == 0){
+      return;
+    }
+
+    for (var i = 0; i < this.Players.length; i++) {
+      if (!players.includes(i)) {
+        this.Score.Scores[this.Players[i]].push(0);
+        //console.log('pushing',0);
+      }
+      else {
+        this.Score.Scores[this.Players[i]].push((this.sum(this.Players[i]) - highestScore - 1) * -1);
+        //console.log('pushing', (this.sum(this.Players[i]) - highestScore - 1) * -1);
+      }
+    }
+    this.rejoinedPlayers=[];
+  }
+
+  nextDistributor() {
     this.distributor = (this.distributor + 1) % this.Players.length;
   }
   ClearStorage() {
@@ -77,14 +154,14 @@ export class AppComponent implements OnInit {
     }, 0) : null;
   }
   DeleteScore() {
-    if (this.Score.Games && this.Score.Games > 0) {
-      for (var i = 0; i < this.Players.length; i++) {
-        this.Score.Scores[this.Players[i]].pop();
-      }
-      this.Score.Games--;
-      this.updateScore();
-      this.distributor = (this.distributor + this.Players.length -1) % this.Players.length;
+    var isRejoin = false;
+    for (var i = 0; i < this.Players.length; i++) {
+      if(this.Score.Scores[this.Players[i]][this.Score.Scores[this.Players[i]].length-1] < 0){isRejoin = true;}
+      this.Score.Scores[this.Players[i]].pop();
     }
+    if(!isRejoin){ this.Score.Games--; this.distributor = (this.distributor + this.Players.length - 1) % this.Players.length;}
+    this.checkRejoin();
+    this.updateScore();    
   }
   RemovePlayer(player: string) {
     this.Players = this.Players.filter(a => a.toLowerCase() != player.toLowerCase());
@@ -98,4 +175,16 @@ export class AppComponent implements OnInit {
     localStorage.setItem('score', JSON.stringify(this.Score));
   }
 
+  getTotalpoints() {
+    this.modalService.open(this.totalPointsModal, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      (result) => {
+        this.totalpoints = result;
+        localStorage.setItem('totalScore', result);
+      },
+      () => {
+        this.getTotalpoints();
+      },
+    );
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {NgbModalBackdrop } from '@ng-bootstrap/ng-bootstrap/modal/modal-backdrop';
 
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
