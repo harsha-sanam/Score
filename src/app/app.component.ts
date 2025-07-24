1a79201510365e42b0a7c7a5c881bf5e96a92f88
harsha-sanam <34478540+harsha-sanam@users.noreply.github.com>
2025-07-23 23:00:26 -0700

    improved ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-
-import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+// app.component.ts
+import { Component, OnInit, TemplateRef, ViewChild, ElementRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { MyMonitoringService } from './my-monitoring-service.service';
 
 @Component({
@@ -12,182 +12,173 @@ export class AppComponent implements OnInit {
   title = 'Score';
   newPlayer: string;
   Players: string[] = [];
-  distributor: number = 0;
+  distributor = 0;
   closeResult = '';
-  totalpoints: number = 0;
+  totalpoints = 0;
   rejoinPlayers: number[] = [];
   rejoinedPlayers: number[] = [];
   Score: any = {};
+  resetCountdown = 5;
+  resetInterval: any;
 
-  ppp: number[] = [];
+  @ViewChild('totalPointsModal', { static: true }) totalPointsModal: TemplateRef<any>;
+  @ViewChild('rejoinModal', { static: true }) rejoinModal: TemplateRef<any>;
+  @ViewChild('resetModal', { static: true }) resetModal: TemplateRef<any>;
+  @ViewChild('scoreBody', { static: false }) scoreBody: ElementRef;
 
-  @ViewChild('totalPoints', { static: true }) totalPointsModal: TemplateRef<any>;
-  @ViewChild('rejoin', { static: true }) rejoinModal: TemplateRef<any>;
+  @ViewChild('scoreTableWrapper', { static: false }) scoreTableWrapper!: ElementRef<HTMLDivElement>;
+@ViewChild('latestScoreRow', { static: false }) latestScoreRow!: ElementRef<HTMLTableRowElement>;
 
-
-  constructor(private modalService: NgbModal,private myMonitoringService: MyMonitoringService) { }
+  constructor(private modalService: NgbModal, private myMonitoringService: MyMonitoringService) { }
 
   ngOnInit() {
-    this.Players = JSON.parse(localStorage.getItem('players')) ? JSON.parse(localStorage.getItem('players')) : [];
-    this.Score = JSON.parse(localStorage.getItem('score'));
-    this.totalpoints = localStorage.getItem('totalScore') ? Number(localStorage.getItem('totalScore')) : 0;
-    if (!this.Score) {
-      this.InitScore();
-    }
-    if (this.totalpoints == 0)
-      this.getTotalpoints();
-    console.log(this.Players);
-    console.log(this.Score);
-
+    this.Players = JSON.parse(localStorage.getItem('players')) || [];
+    this.Score = JSON.parse(localStorage.getItem('score')) || {};
+    this.totalpoints = Number(localStorage.getItem('totalScore')) || 0;
+    if (!this.Score.Scores) this.InitScore();
+    if (this.totalpoints === 0) this.getTotalpoints();
   }
 
-  AddScore() {
-    if (this.Players && this.Players.length > 0) {
-      for (var i = 0; i < this.Players.length; i++) {
-        var score = Number((<HTMLInputElement>document.getElementById(this.Players[i])).value);
-        this.Score.Scores[this.Players[i]].push(score);
-        (<HTMLInputElement>document.getElementById(this.Players[i])).value = "";
-      }
-      this.Score.Games = this.Score.Games + 1;
 
+  scrollToBottom() {
+    setTimeout(() => {
+      if (this.scoreTableWrapper && this.scoreTableWrapper.nativeElement) {
+        const wrapper = this.scoreTableWrapper.nativeElement;
+        wrapper.scrollTop = wrapper.scrollHeight;
+      }
+    }, 100);
+  }
+
+
+  AddScore() {
+    if (this.Players && this.Players.length) {
+      this.Players.forEach(p => {
+        const score = Number((<HTMLInputElement>document.getElementById(p)).value);
+        this.Score.Scores[p].push(score);
+        (<HTMLInputElement>document.getElementById(p)).value = "";
+      });
+      this.Score.Games++;
       this.checkRejoin();
       this.updateScore();
       this.nextDistributor();
+      this.myMonitoringService.logEvent("AddScore", this.Score);
 
-      this.myMonitoringService.logEvent("AddScore",JSON.parse(localStorage.getItem('score')));
-    }
-    else {
+    } else {
       alert('Add players before adding score');
     }
-
   }
+
   checkRejoin() {
-
     this.rejoinPlayers = [];
-    var highestScore = -1;
-    for (var i = 0; i < this.Players.length; i++) {
-      var sum = this.sum(this.Players[i]);
-      if (sum >= this.totalpoints) {
-        this.rejoinPlayers.push(i);
-        continue;
-      }
-      if (sum < this.totalpoints && highestScore < sum) {
-        highestScore = sum;
-      }
-    }
+    let highestScore = -1;
+    this.Players.forEach((p, i) => {
+      const sum = this.sum(p);
+      if (sum >= this.totalpoints) this.rejoinPlayers.push(i);
+      else if (sum > highestScore) highestScore = sum;
+    });
 
-    console.log('Highest Score', highestScore);
+    if (this.rejoinPlayers.length === 0) return;
 
-    if (this.rejoinPlayers.length == 0) { return; }
-
-
-    this.modalService.open(this.rejoinModal, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+    this.modalService.open(this.rejoinModal).result.then(
       () => {
-        this.rejoinPlayers = []; 
+        this.rejoinPlayers = [];
         this.rejoin(this.rejoinedPlayers, highestScore);
       },
-      () => {
-        this.checkRejoin();
-      },
+      () => this.checkRejoin()
     );
   }
+
   onChange(p: number, isChecked: boolean) {
-    if (isChecked) {
-      this.rejoinedPlayers.push(p);
-    } else {
-      let index = this.rejoinedPlayers.indexOf(p);
-      this.rejoinedPlayers.splice(index, 1);
-    }
+    isChecked ? this.rejoinedPlayers.push(p) : this.rejoinedPlayers.splice(this.rejoinedPlayers.indexOf(p), 1);
     this.rejoinedPlayers = [...new Set(this.rejoinedPlayers)];
   }
 
   rejoin(players: number[], highestScore: number) {
+    if (!players || players.length === 0) return;
 
-    if(players == null || players.length == 0){
-      return;
-    }
+    this.Players.forEach((p, i) => {
+      this.Score.Scores[p].push(players.includes(i) ? (this.sum(p) - highestScore - 1) * -1 : 0);
+    });
 
-    for (var i = 0; i < this.Players.length; i++) {
-      if (!players.includes(i)) {
-        this.Score.Scores[this.Players[i]].push(0);
-        //console.log('pushing',0);
-      }
-      else {
-        this.Score.Scores[this.Players[i]].push((this.sum(this.Players[i]) - highestScore - 1) * -1);
-        //console.log('pushing', (this.sum(this.Players[i]) - highestScore - 1) * -1);
-      }
-    }
-    this.rejoinedPlayers=[];
+    this.rejoinedPlayers = [];
   }
 
   nextDistributor() {
     this.distributor = (this.distributor + 1) % this.Players.length;
   }
+
   ClearStorage() {
     localStorage.clear();
+    location.reload();
   }
+
   AddPlayer() {
-    if (!this.newPlayer || this.newPlayer == '' || this.newPlayer == ' ') {
-      return;
-    }
-    if (this.Score.Games && this.Score.Games != 0 && !confirm("All Data will be lost")) {
-      return;
-    }
-    if (this.Players.indexOf(this.newPlayer) >= 0) {
-      return;
-    }
+    if (!this.newPlayer || this.newPlayer.trim() === '' || (this.Score.Games && !confirm("All Data will be lost"))) return;
+    
+    if (this.Players.includes(this.newPlayer)) return;
+
     this.Players.push(this.newPlayer);
     this.newPlayer = "";
     this.updatePlayers();
     this.InitScore();
-    this.distributor = this.Players.length - 1;
   }
+
   InitScore() {
-    this.Score = {};
-    this.Score.Scores = {};
-    this.Score.Games = 0;
-    for (var i = 0; i < this.Players.length; i++) {
-      this.Score.Scores[this.Players[i]] = [];
-    }
+    this.Score = { Scores: {}, Games: 0 };
+    this.Players.forEach(p => this.Score.Scores[p] = []);
     this.updateScore();
   }
-  sum(player: string) {
-    return this.Score.Scores[player] ? this.Score.Scores[player].reduce(function (a, b) {
-      return a + b;
-    }, 0) : null;
+
+  sum(player: string): number {
+    return this.Score.Scores[player] ? this.Score.Scores[player].reduce((a, b) => a + b, 0) : 0;
   }
+
   DeleteScore() {
-    var isRejoin = false;
-    for (var i = 0; i < this.Players.length; i++) {
-      if(this.Score.Scores[this.Players[i]][this.Score.Scores[this.Players[i]].length-1] < 0){isRejoin = true;}
-      this.Score.Scores[this.Players[i]].pop();
+    let isRejoin = false;
+    this.Players.forEach(p => {
+      const last = this.Score.Scores[p].pop();
+      if (last < 0) isRejoin = true;
+    });
+    if (!isRejoin) {
+      this.Score.Games--;
+      this.distributor = (this.distributor + this.Players.length - 1) % this.Players.length;
     }
-    if(!isRejoin){ this.Score.Games--; this.distributor = (this.distributor + this.Players.length - 1) % this.Players.length;}
-    this.checkRejoin();
-    this.updateScore();    
+    
+    this.updateScore();
   }
+
   RemovePlayer(player: string) {
-    this.Players = this.Players.filter(a => a.toLowerCase() != player.toLowerCase());
+    this.Players = this.Players.filter(p => p.toLowerCase() !== player.toLowerCase());
     this.updatePlayers();
   }
 
   updatePlayers() {
     localStorage.setItem('players', JSON.stringify(this.Players));
   }
+
   updateScore() {
     localStorage.setItem('score', JSON.stringify(this.Score));
   }
 
   getTotalpoints() {
-    this.modalService.open(this.totalPointsModal, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-      (result) => {
-        this.totalpoints = result;
-        localStorage.setItem('totalScore', result);
-      },
-      () => {
-        this.getTotalpoints();
-      },
-    );
+    this.modalService.open(this.totalPointsModal).result.then(result => {
+      this.totalpoints = result;
+      localStorage.setItem('totalScore', result);
+    }, () => this.getTotalpoints());
+  }
+
+  openResetModal() {
+    this.resetCountdown = 5;
+    this.modalService.open(this.resetModal);
+    this.resetInterval = setInterval(() => {
+      if (this.resetCountdown > 0) this.resetCountdown--;
+      else clearInterval(this.resetInterval);
+    }, 1000);
+  }
+
+  confirmReset(modal: any) {
+    clearInterval(this.resetInterval);
+    modal.close();
+    this.ClearStorage();
   }
 }
-
